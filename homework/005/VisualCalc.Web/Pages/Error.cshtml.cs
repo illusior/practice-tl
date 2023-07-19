86347a3bbc316b3a4ca2869d97eb3343ae9d006e
illusior <grandoblivioniv@gmail.com>
2023-07-19 19:54:15 +0300

    refactor(`homework`, `№5`): better project name

--- homework/005/VisualCalc.Web/Pages/Error.cshtml.cs
+++ homework/005/VisualCalc.Web/Pages/Error.cshtml.cs
@@ -1,8 +1,11 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace VisualCalc.Pages
+namespace VisualCalc.Web.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     [IgnoreAntiforgeryToken]
