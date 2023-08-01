b2cfa35dcfc309a7f2a57717888e21bbd82e5dd9
illusior <grandoblivioniv@gmail.com>
2023-08-01 20:19:09 +0300

    fix(homework, №2): better vars naming #2

--- homework/002/Shapes/Program.cs
+++ homework/002/Shapes/Program.cs
@@ -1,20 +1,17 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
+﻿using shapes.Shapes;
 
-using shapes.Shapes;
+Triangle trriangle = new(new Point(2, 4), new Point(3, -6), new Point(7, 8));
+Square square = new(new Point(0, 0), 10);
+Circle circle = new(new Point(0, 0), 500);
 
-Triangle tr = new(new Point(2, 4), new Point(3, -6), new Point(7, 8));
-Square sq = new(new Point(0, 0), 10);
-Circle cr = new(new Point(0, 0), 500);
-
-List<IShape> shapes = new List<IShape>()
+List<IShape> shapes = new()
 {
-    tr,
-    sq,
-    cr,
+    trriangle,
+    square,
+    circle,
 };
 
-foreach (var item in shapes)
+foreach (IShape item in shapes)
 {
     Console.WriteLine($"{item.ToString()!.Split('.').Last()}'s area: {item.GetArea()}");
     Console.WriteLine($"{item.ToString()!.Split('.').Last()}'s perimeter: {item.GetPerimeter()}");
